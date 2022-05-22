8710d7364ee086218dfa91dcf9a504ffed831438
Nazmul Hasan <63312173+NazmulHasan7@users.noreply.github.com>
2022-05-23 00:29:01 +0600

    Delete Lab__1 directory

--- Lab__1/src/Task1.java
+++
@@ -1,7 +0,0 @@
-
-public class Task1 {
-
-	public static void main(String[] args) {
-		System.out.println ("Welcome to Java \nWelcime to Computer programming \nProgramming is fun");
-	}
-}
--- Lab__1/src/Task2.java
+++
@@ -1,8 +0,0 @@
-
-public class Task2 {
-	public static void main(String[] args) {
-		
-		for(int i=0 ; i<5; i++) 
-               System.out.println ("Welcome to Java");
-	}
-}
